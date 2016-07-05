a8d2d1c00dda3ee2355c3326349c54f75c88abc5
YuanHu <yuanhu@outlook.com>
2016-07-05 22:43:27 +0800

    Sequence.java源码解析

--- disruptor_src/Sequence.java
+++ disruptor_src/Sequence.java
@@ -19,17 +19,25 @@ import sun.misc.Unsafe;
 
 import com.lmax.disruptor.util.Util;
 
-
+/**
+ * 左边缓存行填充.
+ */
 class LhsPadding
 {
     protected long p1, p2, p3, p4, p5, p6, p7;
 }
 
+/**
+ * value, 需要保存的真正的值(表示生产者/消费者在RingBuffer上的位置).
+ */
 class Value extends LhsPadding
 {
     protected volatile long value;
 }
 
+/**
+ * 右边缓存行填充.
+ */
 class RhsPadding extends Value
 {
     protected long p9, p10, p11, p12, p13, p14, p15;
@@ -43,17 +51,21 @@ class RhsPadding extends Value
  * <p>Also attempts to be more efficient with regards to false
  * sharing by adding padding around the volatile field.
  */
+ /**
+ * 主要用于记录/追踪生产者和消费者在RingBuffer上的位置.
+ */
 public class Sequence extends RhsPadding
 {
     static final long INITIAL_VALUE = -1L;
-    private static final Unsafe UNSAFE;
-    private static final long VALUE_OFFSET;
+    private static final Unsafe UNSAFE;// 主要用于实现原子操作.
+    private static final long VALUE_OFFSET;// value的内存位置的偏移量, 后面会根据这个偏移量set/get value
 
     static
     {
         UNSAFE = Util.getUnsafe();
         try
         {
+			// 使用UnSafe直接从内存中获取value域的内存位置偏移量.
             VALUE_OFFSET = UNSAFE.objectFieldOffset(Value.class.getDeclaredField("value"));
         }
         catch (final Exception e)
@@ -65,6 +77,9 @@ public class Sequence extends RhsPadding
     /**
      * Create a sequence initialised to -1.
      */
+	 /**
+     * 创建一个Sequence, 初始值为-1.
+     */
     public Sequence()
     {
         this(INITIAL_VALUE);
@@ -75,6 +90,9 @@ public class Sequence extends RhsPadding
      *
      * @param initialValue The initial value for this sequence.
      */
+	/**
+     * 用指定的初始值创建一个Sequence.
+     */
     public Sequence(final long initialValue)
     {
         UNSAFE.putOrderedLong(this, VALUE_OFFSET, initialValue);
@@ -84,6 +102,9 @@ public class Sequence extends RhsPadding
      * Perform a volatile read of this sequence's value.
      *
      * @return The current value of the sequence.
+     */
+	/**
+     * 获取value的值.
      */
     public long get()
     {
@@ -96,6 +117,9 @@ public class Sequence extends RhsPadding
      * store.
      *
      * @param value The new value for the sequence.
+     */
+	/**
+     * 使用UnSafe在指定的内存位置设置value的值.
      */
     public void set(final long value)
     {
@@ -110,6 +134,9 @@ public class Sequence extends RhsPadding
      *
      * @param value The new value for the sequence.
      */
+	/**
+     * 使用UnSafe在指定的内存位置设置value的值.
+     */
     public void setVolatile(final long value)
     {
         UNSAFE.putLongVolatile(this, VALUE_OFFSET, value);
@@ -122,6 +149,9 @@ public class Sequence extends RhsPadding
      * @param newValue The value to update to.
      * @return true if the operation succeeds, false otherwise.
      */
+	/**
+     * CAS操作,先比较value的原值是不是expectedValue,如果是更新为newValue,否则不做任何操作.
+     */
     public boolean compareAndSet(final long expectedValue, final long newValue)
     {
         return UNSAFE.compareAndSwapLong(this, VALUE_OFFSET, expectedValue, newValue);
@@ -131,6 +161,9 @@ public class Sequence extends RhsPadding
      * Atomically increment the sequence by one.
      *
      * @return The value after the increment
+     */
+	/**
+     * 将value的值加一,使用了CAS做原子操作.
      */
     public long incrementAndGet()
     {
@@ -142,6 +175,9 @@ public class Sequence extends RhsPadding
      *
      * @param increment The value to add to the sequence.
      * @return The value after the increment.
+     */
+	/**
+     * 将value增加一个指定的值,使用CAS做原子操作.
      */
     public long addAndGet(final long increment)
     {
