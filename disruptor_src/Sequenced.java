976ae4af3c015deffaac69d7531f34d7bfb21ba7
YuanHu <yuanhu@outlook.com>
2016-07-06 20:16:31 +0800

    Sequenced源码分析

--- disruptor_src/Sequenced.java
+++ disruptor_src/Sequenced.java
@@ -7,6 +7,9 @@ public interface Sequenced
      *
      * @return the size of the RingBuffer.
      */
+    /**
+     * 返回当前RingBuffer的大小.
+     */
     int getBufferSize();
 
     /**
@@ -16,6 +19,9 @@ public interface Sequenced
      * @param requiredCapacity in the buffer
      * @return true if the buffer has the capacity to allocate the next sequence otherwise false.
      */
+    /**
+     * 判断当前的RingBuffer是否还有足够的空间可以容纳requiredCapacity个Event.
+     */
     boolean hasAvailableCapacity(final int requiredCapacity);
 
     /**
@@ -23,12 +29,18 @@ public interface Sequenced
      *
      * @return The number of slots remaining.
      */
+    /**
+     * 返回当前RingBuffer可用的空间数目.
+     */
     long remainingCapacity();
 
     /**
      * Claim the next event in sequence for publishing.
      *
      * @return the claimed sequence value
+     */
+    /**
+     * 返回当前RingBuffer上可以给生产者发布Event的位置的序号.
      */
     long next();
 
@@ -48,6 +60,19 @@ public interface Sequenced
      * @param n the number of sequences to claim
      * @return the highest claimed sequence value
      */
+    /**
+     * 向RingBuffer申请n个可用空间给生产者发布Event.主要用于批量发布的场景,使用该函数需要做一些额外的计算,
+     * 比如: 如果需要申请的个数n=10, 则调用该函数之后会返回最后一个可以使用的位置的需要high:
+     * long high = next(10);
+     * 拿到high之后就可以算出第一个可以用的位置的序号low:
+     * long low = high - (n - 1);
+     * 最后生产者需要向[low, high]这个区间的位置填充Event:
+     * for (int i = low; i <= high; i++) {
+     * // 填充该位置的数据.
+     * }
+     * 最后再通知消费者这些区间内的数据可以被消费:
+     * ringBuffer.publish(low, high);
+     */
     long next(int n);
 
     /**
@@ -57,6 +82,9 @@ public interface Sequenced
      *
      * @return the claimed sequence value
      * @throws InsufficientCapacityException
+     */
+    /**
+     * 尝试向RingBuffer申请一个可用空间, 如果有,则返回该可用空间的位置序号,否则抛出异常.
      */
     long tryNext() throws InsufficientCapacityException;
 
@@ -70,12 +98,18 @@ public interface Sequenced
      * @return the claimed sequence value
      * @throws InsufficientCapacityException
      */
+    /**
+     * 尝试向RingBuffer申请n个可用空间,如果有,则返回这些可用空间中最后一个空间的位置序号,否则抛出异常.
+     */
     long tryNext(int n) throws InsufficientCapacityException;
 
     /**
      * Publishes a sequence. Call when the event has been filled.
      *
      * @param sequence
+     */
+    /**
+     * 发布该位置的Event(通知消费者可以消费了), 需要注意的是调用该函数之前需要先将该位置的数据填充上.
      */
     void publish(long sequence);
 
@@ -84,6 +118,9 @@ public interface Sequenced
      *
      * @param lo first sequence number to publish
      * @param hi last sequence number to publish
+     */
+    /**
+     * 发布[lo, hi]区间的Event(通知消费者可以消费了),需要注意的是调用该函数之前需要先将这些位置的数据填充上
      */
     void publish(long lo, long hi);
 }
